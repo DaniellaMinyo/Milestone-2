c84aadc454f5957563b8f27db5ee594d03a0a026
Dany <daniellaminyo@Daniellas-MacBook-Pro.local>
2021-04-20 17:10:09 +0100

    add functionality to video content

---
+++ assets/js/scrollreveal.js
@@ -0,0 +1,17 @@
+//=============Start ScrollReveal --Scroll on move-- Credit to Julian Lloyd https://github.com/jlmakes/scrollreveal======//
+window.sr = ScrollReveal();
+sr.reveal(".move-from-back" , {
+  duration: 6000,
+  origin: "back"
+});
+sr.reveal(".move-from-back2" , {
+  duration: 9000,
+  origin: "back",
+});
+
+sr.reveal(".delay" , {
+  duration: 2000,
+  delay: 3000,
+  origin: "top",
+});
+//=============End ScrollReveal --Scroll on move-- ======//
