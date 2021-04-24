648a142c0df1a429ef9fbef5b9ce12ba8942ccf1
Dany <daniellaminyo@Daniellas-MacBook-Pro.local>
2021-04-24 07:56:41 +0100

    fix bad value errors

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,23 +5,24 @@ let counter = 0;
 let cardPair = [];
 let sec = 0;
 let flip = 0;
+let card = document.querySelectorAll('.card-outer');
 
 cards.forEach((card) => {
-    card.state = "unclicked"
+    card.state = "unclicked";
 });
 
-shuffle()
+shuffle();
 
 for (let  i = 0; i < cards.length; i++) {
     cards[i].addEventListener("click", () => {
         //if first click is false
         if (!firstClick){time()}//call time function
-        firstClick = true //time function called only ones
+        firstClick = true; //time function called only ones
         
         if (cards[i].state == "unclicked") { //if the state is unclicked rotate the clicked card
-            cards[i].style.transform = "rotateY(180deg)"
-            cards[i].state = "clicked"
-            counter++ //increment counter value
+            cards[i].style.transform = "rotateY(180deg)";
+            cards[i].state = "clicked";
+            counter++; //increment counter value
             cardPair.push(cards[i])
             check()//check same src property
         }
@@ -80,7 +81,7 @@ function time() {
     //initalized a variable
     let secs = 0;
     //update the time every one second
-    ID = setInterval(() => {
+    let ID = setInterval(() => {
         secs++
         //display time
         document.querySelector("#timer").innerHTML = secs + "s";
@@ -91,68 +92,10 @@ function time() {
     }
     }, 1000);
 }
-
-//shuffle the cards
+ //shuffle the cards
 function shuffle() {
-    let images = document.querySelectorAll("img");
-    //array of images
-    let srcs = ['assets/images/card01.jpg',
-                'assets/images/card02.jpg',
-                'assets/images/card03.jpg',
-                'assets/images/card04.jpg',
-                'assets/images/card05.jpg',
-                'assets/images/card06.jpg',
-                'assets/images/card07.jpg',
-                'assets/images/card08.jpg',
-                'assets/images/card09.jpg',
-                'assets/images/card10.jpg',
-                'assets/images/card01.jpg',
-                'assets/images/card02.jpg',
-                'assets/images/card03.jpg',
-                'assets/images/card04.jpg',
-                'assets/images/card05.jpg',
-                'assets/images/card06.jpg',
-                'assets/images/card07.jpg',
-                'assets/images/card08.jpg',
-                'assets/images/card09.jpg',
-                'assets/images/card10.jpg'
-                ]
-    for (let i = srcs.length-1; i > 0; i--) {
-        let j = Math.floor(Math.random() * i)
-        let temp = srcs[i]
-        srcs[i] = srcs[j]
-        srcs[j] = temp
-    }
-    //add images to document
-    for (let i = 0; i<images.length; i++) {
-        images[i].src = srcs[i]
-    }
-
-    // add alts to images
-   for (i=1; i < 21; i++) {
-        let j = 0;
-        if (i<= 9){
-            j = "0" + i.toString()
-        }
-        if (i === 10){
-            j = i;
-        }
-        if (i > 10 && i < 20) {
-            j = "0" +(i - 10).toString();
-        }
-        if (i===20) {
-            j = i - 10;
-        }
-        // code from stackoverflow    
-        jQuery(document).ready(function(){
-            let imgs = "assets/images/card"+j+".jpg";
-            jQuery("img").each(function() {
-                if(jQuery(this).attr("src") == imgs)
-                {
-                    jQuery(this).attr("alt", "Card"+j);
-                }
-            });
-
-        });
-    }
-}        
+    card.forEach(cards => {
+        let randomPosition = Math.floor(Math.random() * 30);
+        cards.style.order = randomPosition;
+    })
+}
